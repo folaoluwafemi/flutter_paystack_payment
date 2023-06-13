ea02922aa88c0fc2287b8fe1c38372a71536f260
folaoluwfemi <folaoluwafemi55@gmail.com>
2023-06-13 17:35:13 +0100

    update all dependencies and make tests pass

--- lib/src/ui/webview.dart
+++ lib/src/ui/webview.dart
@@ -3,7 +3,7 @@ import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart' as view;
 
-String? response = "nulll";
+String? response = "null";
 Future<String?> value() async {
   // if (response!.isEmpty) {
   //   return "";
